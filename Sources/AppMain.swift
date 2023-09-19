f7a8df40ae7889c6060d4549ab92c11073ba492f
Ulisse Mini <ulisse.mini@gmail.com>
2023-09-19 15:38:49 +0900

    Silly mvp. clicking notifs still doesn't work

--- Sources/AppMain.swift
+++ Sources/AppMain.swift
@@ -58,30 +58,48 @@ func centerWindow(window: NSWindow) {
     window.setFrameOrigin(NSPoint(x: x + screenRect.minX, y: y + screenRect.minY))
 }
 
-func showNotification() {
+
+func showNotification(title: String, body: String) {
     UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { granted, _ in
         print("Permission granted: \(granted)")
         guard granted else { return }
 
         // Create notification content
         let content = UNMutableNotificationContent()
-        content.title = "Title"
-        content.body = "Body"
+        content.title = title
+        content.body = body
+        content.categoryIdentifier = "chat_msg_click"
 
         // Create trigger and request
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
         let request = UNNotificationRequest(identifier: "someID", content: content, trigger: trigger)
 
         // Schedule the notification
-        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Error scheduling notification: \(error)")
+            }
+        }
     }
 }
 
 
 // MVP Psudocode:
 // - Move window to center and hide. perhaps add opacity.
-// - Notification
+// - Notification every so often with msg from ai bot
+//   - should only notify if they've been on the same window-concept for a while
+// - Clicking notification opens chat window
+// - Chat window is 1-1 with your AI. notifications for window-changes are paused
+//   while you chat.
 
+
+let systemPrompt = """
+You are a productivity assistant. Every few minutes you will be asked to evaluate what the user is doing,
+If the user is doing something they said they didn't want to do, you should ask them why they are doing it,
+and nicely try to motivate them to work. Otherwise you should simply reply with "Great work!" and nothing else.
+
+Try to understand the user's preferences and motivations, they might have a good reason to add an exception.
+""".trimmingCharacters(in: .whitespaces)
 
 
 struct ContentView: View {
@@ -93,10 +111,10 @@ struct ContentView: View {
         VStack {
             Text((activeWindow != nil ? showWindow(activeWindow!) : "No window"))
             .onAppear {
-                // requestScreenRecordingPermission()
-                // Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-                //     activeWindow = getActiveWindow()
-                // }
+                requestScreenRecordingPermission()
+                Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                    activeWindow = getActiveWindow()
+                }
             }
             .font(.system(size: 20))
             Text(chatText)
@@ -115,36 +133,38 @@ struct ContentView: View {
 
                 // TODO: Make sure this is only spawned once.
                 Task {
+                    await try Task.sleep(nanoseconds: 1 * 1_000_000_000)
                     while true {
-                        await try Task.sleep(nanoseconds: 5 * 1_000_000_000)
                         if activeWindow == nil {
                             print("activeWindow is null or unchanged")
                             continue;
                         }
-                        continue
-                        NSApp.unhide(nil)
 
                         let query = ChatQuery(
                             model: "gpt-3.5-turbo",
                             messages: [
-                                Chat(role: .system, content: "Is the window titled \(showWindow(activeWindow!)) useful for coding?"),
+                                Chat(role: .system, content: systemPrompt),
+                                Chat(role: .user, content: "I'd like to be coding right now."),
+                                Chat(role: .user, content: "The user is on a window titled: \(showWindow(activeWindow!)))")
                             ],
-                            maxTokens: 64
+                            maxTokens: 32
                         )
 
                         chatText = ""
                         for try await result in openAI.chatsStream(query: query) {
-                            print(result)
                             chatText += result.choices[0].delta.content ?? ""
                         }
+                        if !chatText.starts(with: "Great work!") {
+                            showNotification(title: "BossGPT", body: chatText)
+                        }
+                        print("chatText: \(chatText)")
 
-                        await try Task.sleep(nanoseconds: 3 * 1_000_000_000)
-                        NSApp.hide(nil)
+                        await try Task.sleep(nanoseconds: 30 * 1_000_000_000)
                     }
                 }
             }
-            Button("Show notification") {
-                showNotification()
+            Button("Test notification") {
+                showNotification(title: "title", body: "body")
             }
         }
         .frame(width: 200, height: 100)
@@ -153,8 +173,39 @@ struct ContentView: View {
 
 }
 
+
+class NotificationDelegate: NSObject, UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("foreground notification")
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+
+        print(response)
+        if response.actionIdentifier == UNNotificationDefaultActionIdentifier {
+            print("Default action!")
+        }
+        if response.actionIdentifier == "CLICK_ACTION" {
+            print("They clicked our special button!")
+        }
+        completionHandler()
+    }
+}
+
+
+
 @main
 struct bossgptApp: App {
+    init() {
+        // Register delegate to handle notification actions
+        UNUserNotificationCenter.current().delegate = NotificationDelegate()
+
+        // Create click action category
+        let clickAction = UNNotificationAction(identifier: "CLICK_ACTION", title: "Click Me", options: [])
+        let category = UNNotificationCategory(identifier: "chat_msg_click", actions: [clickAction], intentIdentifiers: [], options: [])
+        UNUserNotificationCenter.current().setNotificationCategories([category])
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
